705432a2fe2ec7e949a9517fb0124528d8231c12
Yogen P <firacloudtech@gmail.com>
2023-07-01 12:28:49 +0800

    tailwind and daisy setup (#1)

---
+++ src/routes/login/+layout.svelte
@@ -0,0 +1,16 @@
+<script>
+	/** @type {import('./$types').LayoutData} */
+	export let data;
+</script>
+
+<nav>
+	<ul>
+		<li>Hello</li>
+	</ul>
+</nav>
+
+<main class="card w-4/6 mx-auto bg-neutral text-neutral-content">
+	<div class="card-body items-center text-center">
+		<slot />
+	</div>
+</main>
